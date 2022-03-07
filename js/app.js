b4bf9ca589b6d2668a570f64111b3e39b7231c7d
Aleksandr Borysenko <3pc@ukr.net>
2022-03-07 16:55:57 +0200

    feat: add footer, cv, some icons

--- js/app.js
+++ js/app.js
@@ -62,3 +62,28 @@ else {
 }
 
 window.addEventListener("scroll", changeLinkState);
+
+// const sections = document.querySelectorAll("section[id]");
+// window.addEventListener("scroll", navHighlighter);
+
+// function navHighlighter() {
+  
+//     let scrollY = window.pageYOffset;
+   
+//   sections.forEach(current => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     sectionId = current.getAttribute("id");
+        
+//     if (
+//       scrollY > sectionTop &&
+//       scrollY <= sectionTop + sectionHeight
+//     ){
+//       document.querySelector(".sidebar a[href*=" + sectionId + "]").classList.add("active");
+//     } else {
+//       document.querySelector(".sidebar a[href*=" + sectionId + "]").classList.remove("active");
+//     }
+//   });
+// }
+
+
